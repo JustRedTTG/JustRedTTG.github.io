bb6f3803cc9a661b1b23fdc3dfda634c81f80cec
RedTTG <redtonehair@gmail.com>
2022-11-02 01:29:11 +0200

    controls need a bit of fixing but it works

--- snake.js
+++ snake.js
@@ -6,6 +6,12 @@ var board_size = 25;
 var board_pixel_size;
 var board_element;
 var board_context;
+var board;
+var snake_size;
+var snake_direction;
+var snake_direction_change_chain = [];
+var enable_teleport = true;
+var game_over = false;
 
 function resize() {
   w = $(window).width();
@@ -26,12 +32,28 @@ function resize() {
   board_context.fillRect(0,0, w, h);
 }
 
+function initialize_board() {
+  board = []
+  for(var i = 0; i < board_size; i++) {
+    var arr = []
+    for(var i2 = 0; i2 < board_size; i2++) {
+      arr.push(-2);
+    }
+    board.push(arr);
+  }
+  board[Math.floor(board_size/3)][Math.floor(board_size/2)] = 1;
+  board[Math.floor(board_size - board_size/3)][Math.floor(board_size/2)] = -1;
+  snake_size = 3;
+  snake_direction = 1;
+}
+
 window.onload = function() {
   console.log("Snake By Red");
   board_element = document.getElementById("canvas");
   board_context = board_element.getContext("2d");
   resize();
-  setInterval(game, 100)
+  initialize_board();
+  setInterval(game, 100);
 }
 
 window.addEventListener("resize", () => {
@@ -43,8 +65,36 @@ function game() {
   board_context.fillStyle = "#202020";
   board_context.fillRect(0,0, w, h);
 
+  draw_board();
+  if (!game_over){move_snake();}
+}
+
+function draw_board() {
   board_context.fillStyle = "black";
   board_context.fillRect(offset_x,offset_y, board_size * board_pixel_size, board_size * board_pixel_size);
+
+  var x_in_pixel = offset_x;
+  var y_in_pixel = offset_y;
+  for (var x = 0; x < board_size; x++) {
+    for (var y = 0; y < board_size; y++) {
+      var v = board[x][y];
+      if (v == -1) {
+        board_context.strokeStyle = "red";
+        board_context.fillStyle = "red";
+        board_context.beginPath();
+        board_context.ellipse(x_in_pixel + board_pixel_size / 2, y_in_pixel + board_pixel_size / 2, board_pixel_size / 2, board_pixel_size / 2, 0, 0, 2 * Math.PI);
+        board_context.fill();
+        board_context.stroke();
+      }
+      else if (0 < v && v <= snake_size) {
+        board_context.fillStyle = "green";
+        board_context.fillRect(x_in_pixel, y_in_pixel, board_pixel_size, board_pixel_size);
+      }
+      y_in_pixel += board_pixel_size;
+    }
+    x_in_pixel += board_pixel_size;
+    y_in_pixel = offset_y;
+  }
 
   board_context.strokeStyle = "white";
   board_context.beginPath();
@@ -52,3 +102,97 @@ function game() {
   board_context.lineWidth = h / 200;
   board_context.stroke();
 }
+
+function move_snake() {
+  if (snake_direction_change_chain.length > 0) {
+    snake_direction = snake_direction_change_chain.shift();
+  }
+
+  var [next_xi, next_yi, teleported] = get_next_snake_position();
+
+  for (var xi = 0; xi < board_size; xi++) {
+    for (var yi = 0; yi < board_size; yi++) {
+      if (board[xi][yi] > 0) {board[xi][yi] ++;}
+  }}
+  if (teleported && !enable_teleport) {game_over = true;}
+  else if (1 < board[next_xi][next_yi] && board[next_xi][next_yi] <= snake_size) {game_over = true;}
+  else {
+    if (board[next_xi][next_yi] == -1) {
+      snake_size ++;
+      board[next_xi][next_yi] = 1;
+      generate_new_apple();
+    }
+    else {board[next_xi][next_yi] = 1;}
+  }
+}
+
+function get_next_snake_position() {
+  var [xi, yi] = find_snake_head();
+  if (snake_direction == 0){yi -= 1;}
+  else if (snake_direction == 1){xi += 1;}
+  else if (snake_direction == 2){yi += 1;}
+  else if (snake_direction == 3){xi -= 1;}
+
+  if (xi < 0){xi = board_size-1;}
+  else if (xi >= board_size){xi = 0;}
+  else if (yi < 0){yi = board_size-1;}
+  else if (yi >= board_size){yi = 0;}
+  else {return [xi, yi, false];}
+  return [xi, yi, true];
+}
+
+function find_snake_head() {
+  for (var xi = 0; xi < board_size; xi++) {
+    for (var yi = 0; yi < board_size; yi++) {
+      if (board[xi][yi] == 1) {return [xi, yi];}
+    }
+  }
+  console.log("Couldn't find the snake");
+  return [0, 0];
+}
+
+function generate_new_apple() {
+  var [xi, yi] = find_snake_head();
+  var [new_xi, new_yi] = [xi, yi];
+  while ((xi-2 <= new_xi && new_xi <= xi+2) || (yi-2 <= new_yi && new_yi <= yi+2) || (1 <= board[new_xi][new_yi] && board[new_xi][new_yi] <= snake_size)) {
+    new_xi = Math.floor(Math.random() * board_size);
+    new_yi = Math.floor(Math.random() * board_size);
+  }
+  board[new_xi][new_yi] = -1;
+}
+
+document.addEventListener('keydown', function(event) {
+  var new_snake_direction = -1;
+
+  if (event.keyCode == 37 || event.keyCode == 65) { // Right
+      new_snake_direction = 3;
+  }
+  else if (event.keyCode == 39 || event.keyCode == 68) { // Left
+      new_snake_direction = 1;
+  }
+  else if (event.keyCode == 38 || event.keyCode == 87) { // Up
+      new_snake_direction = 0;
+  }
+  else if (event.keyCode == 40 || event.keyCode == 83) { // Down
+      new_snake_direction = 2;
+  }
+
+  if (new_snake_direction < 0 || game_over) {return;}
+  event.preventDefault();
+
+  var old_snake_direction = snake_direction
+
+  if (snake_direction_change_chain.length > 0) {
+    if (snake_direction_change_chain[snake_direction_change_chain.length-1] == new_snake_direction) {return;}
+  else if (snake_direction == new_snake_direction) {return;}
+  }
+  if (snake_direction_change_chain.length > 0) {
+    snake_direction = snake_direction_change_chain[snake_direction_change_chain.length-1];
+  } else {snake_direction = new_snake_direction;}
+
+  var [xi, yi, teleported] = get_next_snake_position();
+  snake_direction = old_snake_direction;
+  if (teleported) {return;}
+  if (board[xi][yi] != 2) {snake_direction_change_chain.push(new_snake_direction)}
+
+});
